131bb307810bdea700093ee7f06d5c939782ed37
Paolo Esposito <paolo.esposito@kalpa.it>
2020-09-16 17:27:37 +0200

    FieldCell Added

---
+++ CampoSminato/FieldCell.swift
@@ -0,0 +1,89 @@
+//
+//  FieldCell.swift
+//  CampoSminato
+//
+//  Created by Paolo Esposito on 16/09/2020.
+//  Copyright © 2020 Paolo Esposito. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FieldCell: UICollectionViewCell {
+    var adiacentBombsCount: Int = 0
+    var status: UIColor
+    var discovered: Bool
+    let bomb: Bool
+    var contentVisible: Bool
+    let contentCellLabel: UILabel
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        adiacentBombsCount = 0;
+        setUnknown()
+        
+        // Number that display the adiacent bombs
+        contentCellLabel = UILabel()
+        contentCellLabel.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height)
+        contentCellLabel.textAlignment = .center
+        contentCellLabel.backgroundColor = .clear
+        addSubview(contentCellLabel)
+        setContentVisible(contentVisible: false)
+
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setAdiacentBombsCount(adiacentBombsCount: Int) {
+        if bomb {return}
+        self.adiacentBombsCount = adiacentBombsCount
+        self.contentCellLabel.text = String(adiacentBombsCount)
+    }
+
+    func setBomb(bomb: Bool){
+        if (bomb) {
+            self.contentCellLabel.text = "B"
+        }
+    }
+
+    
+    func setUnknown() {
+        self.backgroundColor = .darkGray
+    }
+    
+    func setDiscovered(discovered: Bool){
+        if(discovered){
+            // here show numbers
+            self.backgroundColor = .lightGray
+            self.isUserInteractionEnabled = false;
+            status = self.backgroundColor!
+        }else{
+            self.backgroundColor = status
+        }
+        self.discovered = discovered
+        setContentVisible(contentVisible: self.discovered)
+    }
+
+    func showBomb(show: Bool){
+        if(show){
+            self.backgroundColor = .red
+        }else{
+            self.backgroundColor = self.status
+        }
+    }
+    
+    func setContentVisible(contentVisible: Bool){
+        self.contentVisible = contentVisible
+        self.contentCellLabel.isHidden = !contentVisible
+    }
+
+
+
+
+
+}
--- CampoSminato/GameViewController.swift
+++ CampoSminato/GameViewController.swift
@@ -8,3 +8,45 @@
 
 import Foundation
 import UIKit
+
+class GameViewController : UIViewController, BoardDelegate {
+    
+    var myCollectionView:UICollectionView?
+    //let field: Field?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 60, height: 60)
+        
+        myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        myCollectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+        myCollectionView?.backgroundColor = UIColor.white
+        view.addSubview(myCollectionView ?? UICollectionView())
+        
+        self.view = view
+    }
+    
+    // MARK: - Board Delegates
+    
+    func boardCell(_ cell: BoardCell?, didBecomeDiscoveredAt indexPath: IndexPath?) {
+        <#code#>
+    }
+    
+    func userFindBomb(at indexPath: IndexPath?) {
+        <#code#>
+    }
+    
+    func willWin() {
+        <#code#>
+    }
+    
+    func willLose() {
+        <#code#>
+    }
+}
